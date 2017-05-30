8236c141fbfe6bcafebe84ebd96ed670dc2dcd2e
Imad El HItti <imadhy.gt@gmail.com>
2017-05-30 15:48:43 +0200

    Factorize entities form components and modify rest button utility
    Reset button will now reset form to empty value if we are in add mode, or it will reset to the original value of the edited entity if we are in edit mode

--- src/app/car/car-form/car-form.component.ts
+++ src/app/car/car-form/car-form.component.ts
@@ -16,107 +16,90 @@ export class CarFormComponent implements OnInit {
   // Regex for model year, only digits and length = 4
   readonly YEAR_REGEX = /^[0-9]{4}$/;
 
-  private carInput: Car;
-  carForm: FormGroup;
+  private car: Car;
+  private form: FormGroup;
 
   private id: number;
 
   constructor(
     private _carService: CarService,
     private _route: ActivatedRoute,
-    private _fb: FormBuilder,
+    private _formBuilder: FormBuilder,
     private _notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.id = +this._route.snapshot.params['id'];
-    this.formInitialize(this.id);
+    this.getIdFromRouteParams();
+    this.initForm();
   }
 
-  /**
-   * Function to initialize form values:
-   * If we have an :id parameter sent to the component, the form
-   * will initialize values with the data of the object we got from
-   * the service carService.getSingle().
-   * Otherwise, we initialize an empty form
-   */
-  formInitialize = (id: number) => {
-    // Initialize form with empty values
-    this.emptyForm();
-
-    // If we have an id, get the car and fill the form with its values
-    if (id) {
-      this._carService.getSingle(id)
-        .subscribe((car: Car) => {
-          this.carInput = car;
-          this.formWithCarDetails(this.carInput);
-        },
-        error => {
-          this._notificationService.error('Car not found', 'Couldn\'t find the car with the given id');
-        });
+  getIdFromRouteParams = () => {
+    this.id = this._route.snapshot.params['id'];
+    if (typeof this.id === 'string') {
+      this.id = +this.id;
     }
   }
 
-  emptyForm = () => {
-    // Initialize form with empty values
-    this.carForm = this._fb.group({
-      id: { value: '', disabled: true },
-      maker: ['', Validators.required],
-      model: ['', Validators.required],
-      year: ['', Validators.compose([Validators.required, Validators.pattern(this.YEAR_REGEX)])],
-      driver: ['', Validators.required]
-    });
+  initForm = () => {
+    this.form = this._formBuilder.group(this.getNewForm());
+    if (this.id) {
+      this.load();
+    }
   }
 
-  formWithCarDetails = (car: Car) => {
-    // Initialize Form with Car details
-    this.carForm = this._fb.group({
-      id: { value: car.id, disabled: true },
-      maker: [car.maker, Validators.required],
-      model: [car.model, Validators.required],
-      year: [car.year, Validators.compose([Validators.required, Validators.pattern(this.YEAR_REGEX)])],
-      driver: [car.driver, Validators.required]
-    });
+  getNewForm = (car?: Car) => {
+    return {
+      id: [{
+        value: (car ? car.id : ''),
+        disabled: true
+      }],
+      maker: [
+        (car ? car.maker : ''),
+        Validators.required
+      ],
+      model: [
+        (car ? car.model : ''),
+        Validators.required
+      ],
+      year: [
+        (car ? car.year : ''),
+        Validators.compose([Validators.required, Validators.pattern(this.YEAR_REGEX)])
+      ],
+      driver: [
+        (car ? car.driver : ''),
+        Validators.required
+      ]
+    };
+  }
+
+  load = () => {
+    this._carService.getSingle(this.id)
+        .subscribe((car: Car) => {
+          this.car = car;
+          this.form = this._formBuilder.group(this.getNewForm(car));
+        },
+        error => this._notificationService.error('Car not found', 'Couldn\'t find the car with the given id'));
   }
 
   save = () => {
-    if (!this.carInput) {
-      // If we didn't get a car, call Add function from car service
+    if (!this.car) {
       this.add();
     } else {
       this.update();
     }
   }
 
-  /**
-   * Call CarService with add (POST) method
-   * TODO:
-   * - if success ? what to do
-   */
   add = () => {
-    this._carService.add(this.carForm.value).subscribe(
+    this._carService.add(this.form.value).subscribe(
       result => this._notificationService.success('Success', 'Car added successfuly'),
-      error => {
-        console.error(error);
-        this._notificationService.error('Error', 'An error occured when trying to reach the server');
-      });
+      error => this._notificationService.error('Error', 'An error occured when trying to reach the server')
+    );
   }
 
-  /**
-   * Call CarService with update (PUT) method
-   * TODO:
-   * - if success ? what to do
-   */
   update = () => {
-    this._carService.update(+this.carInput.id, <Car>this.carForm.getRawValue()).subscribe(
+    this._carService.update(+this.car.id, <Car>this.form.getRawValue()).subscribe(
       result => this._notificationService.success('Success', 'Car edited successfuly'),
-      error => {
-        console.error(error);
-        this._notificationService.error('Error', 'An error occured when trying to reach the server');
-      });
+      error => this._notificationService.error('Error', 'An error occured when trying to reach the server')
+    );
   }
 
-  // Function to reset form values and carInput data
-  resetForm() {
-    this.carForm.reset();
-  }
 }
--- src/app/driver/driver-form/driver-form.component.ts
+++ src/app/driver/driver-form/driver-form.component.ts
@@ -13,82 +13,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriverFormComponent implements OnInit {
 
-  private driverInput: Driver;
-  driverForm: FormGroup;
-
+  private driver: Driver;
+  private form: FormGroup;
   private id: number;
 
   constructor(
     private _driverService: DriverService,
     private _route: ActivatedRoute,
     private _router: Router,
-    private _fb: FormBuilder,
+    private _formBuilder: FormBuilder,
     private _notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.id = +this._route.snapshot.params['id'];
-    this.formInitialize(this.id);
+
+    this.getIdFromRouteParams();
+    this.initForm();
   }
 
-  /**
-   * Function to initialize form values:
-   * If we have an :id parameter sent to the component, the form
-   * will initialize values with the data of the object we got from
-   * the service driverService.getSingle().
-   * Otherwise, we initialize an empty form
-   */
-  formInitialize = (id: number) => {
-    // Initialize form with empty values
-    this.emptyForm();
-
-    if (id) {
-      this._driverService.getSingle(+this._route.snapshot.paramMap.get('id'))
-        .subscribe((driver: Driver) => {
-          this.driverInput = driver;
-          this.formWithDriverDetails(this.driverInput);
-        },
-        error => {
-          this._notificationService.error('Driver not found', 'Couldn\'t find the driver with the given id');
-        });
+  getIdFromRouteParams = () => {
+    this.id = this._route.snapshot.params['id'];
+    if (typeof this.id === 'string') {
+      this.id = +this.id;
     }
   }
 
-  emptyForm = () => {
-    // Initialize form with empty values
-    this.driverForm = this._fb.group({
-      id: { value: '', disabled: true },
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      car: ['', Validators.required]
-    });
+  initForm = () => {
+    this.form = this._formBuilder.group(this.getNewForm());
+    if (this.id) {
+      this.load();
+    }
   }
 
-  formWithDriverDetails = (driver: Driver) => {
-    // Initialize Form with Driver details
-    this.driverForm = this._fb.group({
-      id: { value: driver.id, disabled: true },
-      firstName: [driver.firstName, Validators.required],
-      lastName: [driver.lastName, Validators.required],
-      car: [driver.car, Validators.required]
-    });
+  getNewForm = (driver?: Driver) => {
+    return {
+      id: [{
+        value: (driver ? driver.id : ''),
+        disabled: true
+      }],
+      firstName: [
+        (driver ? driver.firstName : ''),
+        Validators.required
+      ],
+      lastName: [
+        (driver ? driver.lastName : ''),
+        Validators.required
+      ],
+      car: [
+        (driver ? driver.car : ''),
+        Validators.required
+      ]
+    };
+  }
+
+  load = () => {
+    this._driverService.getSingle(+this._route.snapshot.paramMap.get('id'))
+        .subscribe((driver: Driver) => {
+          this.driver = driver;
+          this.form = this._formBuilder.group(this.getNewForm(driver));
+        },
+        error => this._notificationService.error('Driver not found', 'Couldn\'t find the driver with the given id'));
   }
 
   save = () => {
     // If we didn't get a driver, we are adding a driver
-    if (!this.driverInput) {
+    if (!this.driver) {
       this.add();
     } else { // If we didn't get a driver, we are adding a driver
       this.update();
     }
   }
 
-  /**
-   * Call DriverService with add (POST) method
-   * TODO:
-   * - if success ? what to do
-   */
   add = () => {
-    this._driverService.add(this.driverForm.value).subscribe(
+    this._driverService.add(this.form.value).subscribe(
       result => this._notificationService.success('Success', 'Driver added successfuly'),
       error => {
         console.error(error);
@@ -96,27 +92,12 @@ export class DriverFormComponent implements OnInit {
       });
   }
 
-  /**
-   * Call DriverService with update (PUT) method
-   * TODO:
-   * - if success ? what to do
-   */
   update = () => {
-    this._driverService.update(+this.driverInput.id, <Driver>this.driverForm.getRawValue()).subscribe(
+    this._driverService.update(+this.driver.id, <Driver>this.form.getRawValue()).subscribe(
       result => this._notificationService.success('Success', 'Driver edited successfuly'),
       error => {
         console.error(error);
         this._notificationService.error('Error', 'An error occured when trying to reach the server');
       });
-  }
-
-  // Function to reset form values and driverInput data
-  resetForm = () => {
-    // this.driverForm.reset();
-    this.goToForm();
-  }
-
-  goToForm() {
-    this._router.navigate(['./driver-form']);
   }
 }
